9a6e1a1d16e664c0ec038a7f856fbf7183303a5e
Raphael Weiner <raphael.weiner@gmail.com>
2013-05-06 10:44:17 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FeedEngine::Application.config.secret_token = '344ccf9be6816e75ab49a6c85319f014321d7607b7666825d0d357e66f06f02d56fabbd92486c0c50bfc3e3d5c44f244945fc7f5731cc757f1d1851f806c48f3'
