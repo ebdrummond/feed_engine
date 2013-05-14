dde825a92d8450bd6eda9b6dca828b7fcbd63aea
Raphael Weiner <raphael.weiner@gmail.com>
2013-05-14 15:25:30 -0600

    small trip refactor

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,14 +5,15 @@ class Trip < ActiveRecord::Base
                   :end,
                   :visible
 
-  belongs_to :owner, :foreign_key => 'user_id', class_name: 'User'
+  belongs_to :owner, :foreign_key => 'user_id', :class_name => 'User'
+
+  validate :end_date_cannot_be_earlier_than_start_date
 
   validates :name, :presence => true
   validates :destination, :presence => true
   validates :start, :presence => true
   validates :end, :presence => true
   validates :owner, :presence => true
-  validate :end_date_cannot_be_earlier_than_start_date
 
   def end_date_cannot_be_earlier_than_start_date
     if self.end.present? && self.start.present? && self.end < self.start
