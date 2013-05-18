5e4e4eee6011b474cdeeddbde0300b01971a1ac3
Raphael Weiner <raphael.weiner@gmail.com>
2013-05-17 19:07:53 -0600

    refactor twitter service spec

--- spec/lib/twitter_service_spec.rb
+++ spec/lib/twitter_service_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe TwitterService do
-  describe '.update' do
+  describe '.retrieve_and_store_tweets_for_user' do
     context 'with invalid token/secret' do
       it 'throws an invalid or expired token' do
         user = User.create!(username: 'Phil')
@@ -10,33 +10,43 @@ describe TwitterService do
                                   :token => 'abc',
                                   :secret => '123')
         VCR.use_cassette('twitter_service_unauthenticated') do
-          expect { TwitterService.update }.to raise_error Twitter::Error::Unauthorized
+          expect {
+            TwitterService.retrieve_and_store_tweets_for_user('user_id' => user.id,
+                                                              'token' => 'abc',
+                                                              'secret' => '123')
+            }.to raise_error Twitter::Error::Unauthorized
         end
       end
     end
 
     context 'with a valid token/secret' do
       before(:each) do
-        user = User.create!(username: 'Phil')
-        user.auth_sources.create!(:provider => 'twitter',
-                                  :uid => 'abc123',
-                                  :token => '57272421-DdrlGSaASFZj1qjoqYqiSo4YDmJkDkaZOBVE5rSV4',
-                                  :secret => 'INXG4Rg7mgtTr2oZQhgjf6O9UJPSPqtv4HbFWsnMg')
+        @user = User.create!(username: 'Phil')
+        @user.auth_sources.create!(:provider => 'twitter',
+                                   :uid => 'abc123',
+                                   :token => '57272421-DdrlGSaASFZj1qjoqYqiSo4YDmJkDkaZOBVE5rSV4',
+                                   :secret => 'INXG4Rg7mgtTr2oZQhgjf6O9UJPSPqtv4HbFWsnMg')
+      end
+
+      let(:retrieve_and_store_tweets_for_user) do
+        TwitterService.retrieve_and_store_tweets_for_user('user_id' => @user.id,
+                                                          'token' => '57272421-DdrlGSaASFZj1qjoqYqiSo4YDmJkDkaZOBVE5rSV4',
+                                                          'secret' => 'INXG4Rg7mgtTr2oZQhgjf6O9UJPSPqtv4HbFWsnMg')
       end
 
       it 'retrieves tweets from twitter and saves them to the database' do
         VCR.use_cassette('twitter_service_one_new_tweet') do
-          expect { TwitterService.update }.to change { Tweet.count }.by 1
+          expect { retrieve_and_store_tweets_for_user }.to change { Tweet.count }.by 8
         end
       end
 
       it "only retrieves tweets that don't exist in database yet" do
         VCR.use_cassette('twitter_service_one_new_tweet') do
-          TwitterService.update
+          retrieve_and_store_tweets_for_user
         end
 
         VCR.use_cassette('twitter_service_no_new_tweets') do
-          expect { TwitterService.update }.to change { Tweet.count }.by 0
+          expect { retrieve_and_store_tweets_for_user }.to change { Tweet.count }.by 0
         end
       end
     end
