0e9d3522fed835515d4be597cf70f66cc5d538e8
Raphael Weiner <raphael.weiner@gmail.com>
2013-05-18 22:30:43 -0600

    travis ci update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,15 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 
+require 'coveralls'
 require 'simplecov'
+Coveralls.wear!('rails')
+
+# SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
   add_filter '/spec'
 end
 
-require 'coveralls'
-Coveralls.wear!
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
