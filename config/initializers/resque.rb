cf32b3d4bfc65cc0e0b79761a8111ba75d75240e
Kyle Suss <susskyle@gmail.com>
2013-05-20 16:33:01 -0600

    added an after hook in the resque initializer in an attempt to get it to work on heroku

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -6,3 +6,4 @@ Resque.redis.namespace = "resque:SchedulerExample"
 require File.join(Rails.root, 'app/jobs/poll_job.rb')
 
 Resque.schedule = YAML.load_file(File.join(Rails.root, 'config/resque_scheduler.yml'))
+Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
