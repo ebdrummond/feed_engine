c47ad889313cf5283f5555bda96c788fc0992ee2
Raphael Weiner <raphael.weiner@gmail.com>
2013-05-22 15:04:39 -0600

    users dates searchable spec

--- spec/lib/users_dates_searchable_spec.rb
+++ spec/lib/users_dates_searchable_spec.rb
@@ -1,17 +1,30 @@
 require 'spec_helper'
 
 describe UsersDatesSearchable do
-  describe '.for_users' do
-    # TODO: Ask Franklin tomorrow
-    xit 'makes one database lookup' do
-      user = double('I am a user!')
-      UsersDatesSearchable.should_receive(:where).with('user_id IN (?)', [user])
+  subject do
+    class TestMixedinSubject
+      extend ::UsersDatesSearchable
+    end
+  end
 
-      UsersDatesSearchable.for_users([user])
+  describe '.for_users' do
+    it 'makes one database lookup with params' do
+      subject.should_receive(:where).with('user_id IN (?)', [1, 2])
+      subject.for_users([1, 2])
     end
   end
 
   describe '.in_range' do
-    it 'does something'
+    it 'makes one database lookup' do
+      subject.stub(:event_created_at).and_return(:created_at)
+      subject.should_receive(:where).with(:created_at => 1..2)
+      subject.in_range(1,2)
+    end
+  end
+
+  describe '.event_created_at' do
+    it 'raises error when not implemented into host class' do
+      expect { subject.event_created_at }.to raise_error NotImplementedError
+    end
   end
 end
