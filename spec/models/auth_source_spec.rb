0f6de2102beefac3ee205abb1c40e008a2264e6a
Raphael Weiner <raphael.weiner@gmail.com>
2013-05-22 15:10:42 -0600

    auth source spec

--- spec/models/auth_source_spec.rb
+++ spec/models/auth_source_spec.rb
@@ -22,7 +22,6 @@ describe AuthSource do
     expect { subject.token = nil }.to change { subject.valid? }.to false
   end
 
-
   it 'requires a uid' do
     expect { subject.uid = nil }.to change { subject.valid? }.to false
   end
@@ -38,10 +37,24 @@ describe AuthSource do
   end
 
   context 'provider is foursquare' do
-    before { subject.provider = 'foursquare' }
-
     it 'does not require a secret' do
+      subject.provider = 'foursquare'
       expect { subject.secret = nil }.to_not change { subject.valid? }.to false
+    end
+  end
+
+  context 'provider is instagram' do
+    it 'does not require a secret' do
+      subject.provider = 'instagram'
+      expect { subject.secret = nil }.to_not change { subject.valid? }.to false
+    end
+  end
+
+  describe '.grouped_by_provider' do
+    it 'returns all auth sources grouped by provider' do
+      subject.save
+
+      expect(AuthSource.grouped_by_provider).to eq({'twitter' => [subject] })
     end
   end
 end
